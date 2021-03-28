c64f9fd37767a7ccdb17a61bd01b0cb28617893b
daibogh <daibogh@gmail.com>
2021-03-28 06:28:53 +0300

    feat (api) removed useless prop

--- src/pages/News/panels/NewsDetail/NewsDetail.jsx
+++ src/pages/News/panels/NewsDetail/NewsDetail.jsx
@@ -18,7 +18,7 @@ import {
   Icon36ChevronLeftOutline,
 } from '@vkontakte/icons';
 
-export const NewsDetail = ({ onBack, newsId, newsInfo }) => {
+export const NewsDetail = ({ onBack, newsInfo }) => {
   if (newsInfo == null) return null;
   return (
     <Panel>
