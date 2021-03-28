897e7c9c187540598eacc3b9f5db7b9cd78a0df7
daibogh <daibogh@gmail.com>
2021-03-28 08:44:25 +0300

    fix (api) data fetching

--- src/api/locationsQuery.js
+++ src/api/locationsQuery.js
@@ -1,9 +1,7 @@
 export const locationsQuery = async ({ city }) => {
   const dataList = await fetch(
     `http://drop-table.tech/api/region/locations?city=${encodeURI(city)}`
-  )
-    .then((resp) => resp.json())
-    .then((resp) => resp?.data);
+  ).then((resp) => resp.json());
   return (dataList || []).map((data) => {
     return {
       id: data?.id,
